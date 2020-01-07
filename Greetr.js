23b86215c4bf8d0cb863264067ca762b69487d76
nljacob <neehaljacob@gmail.com>
2020-01-06 20:22:41 -0800

    added methods

--- Greetr.js
+++ Greetr.js
@@ -4,9 +4,74 @@
         return new Greetr.init(firstname, lastname, language);
     }
 
-    Greetr.prototype = {};
+    var supportedLangs = ['en', 'es'];
 
-    Greeter.init = function(firstname, lastname, language){
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreeting = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inició sesión'
+    };
+
+    Greetr.prototype = {
+        fullName: function(){
+            return this.firstname + ' ' + this.lastname;
+        }, 
+
+        validate: function(){
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw 'Invalid language';
+            }
+        },
+
+        greeting: function(){
+            return greetings[this.language] + ' ' + this.firstname + '!';
+        },
+
+        formalGreetings: function(){
+            return formalGreeting[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal){
+            var msg;
+
+            if(formal){
+                msg = this.formalGreeting();
+            }
+            else{
+                msg = this.greeting();
+            }
+            if(console){
+                console.log(msg);
+            }
+
+            //makes the method chainable
+            return this;
+        },
+
+        log: function(){
+            if(console){
+                console.log(logMessages[this.language + ': '] + this.fullName())
+            }
+            return this;
+        },
+        setLang: function(lang){
+            this.language = lang;
+            this.validate();
+
+            return this;
+        }
+    };
+
+    Greetr.init = function(firstname, lastname, language){
         
         var self = this;
         self.firstname = firstname || '';
@@ -16,5 +81,6 @@
 
     Greetr.init.prototype = Greetr.prototype;
 
-})(window, jQuery)
+    global.Greetr = global.G$ = Greetr;
 
+})(window, jQuery)
