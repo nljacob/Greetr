1d5f8649a8e475642459dbf52369f83a502df5a5
nljacob <neehaljacob@gmail.com>
2020-01-06 18:20:39 -0800

    creating function constructor Greetr.init and setting up default values

---
+++ Greetr.js
@@ -0,0 +1,14 @@
+(function(global, $){
+
+    var Greetr = function(firstname, lastname, language){
+        return new Greetr.init(firstname, lastname, language);
+    }
+
+    Greeter.init = function(firstname, lastname, language){
+        var self = this;
+        self.firstname = firstname || '';
+        self.lastname = lastname || '';
+        self.language = language || 'en'
+    }
+
+})(window, jQuery)
